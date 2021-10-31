4ee81a6698c775be8f5c24d6161af6f069c6ffef
Floice Nyota <nyotafloice@gmail.com>
2021-10-31 15:05:36 +0300

    add serializer

--- awwards/settings.py
+++ awwards/settings.py
@@ -22,6 +22,7 @@ cloudinary.config(
     secure = config('SECURE'),
 )
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -48,6 +49,8 @@ INSTALLED_APPS = [
     'awwards_app',
     'bootstrap3',
     'cloudinary',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
